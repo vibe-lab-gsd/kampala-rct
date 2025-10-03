8d3aff833cf7d293cdf66513ee7a31b2afd517a0
Gray <118842255+gcollins99@users.noreply.github.com>
2025-10-03 10:04:09 -0400

    update sampling - randomize n for odd-n strata

--- code/phase2/randomization.R
+++ code/phase2/randomization.R
@@ -10,17 +10,27 @@ strata <- read_csv(file.path(git_dir, "data", phase, "stage_strata.csv"))
 
 set.seed(192333)
 
-samp <- strata %>% 
-  arrange(branch_code) %>% 
-  group_by(strata) %>% 
-  slice_sample(prop = 0.5) %>%    # select randomly, but order data by branch code, which uniquely identifies rows, for replicability  
-  mutate(treatment = 1)
+samp <-   strata %>%
+  arrange(branch_code) %>%
+  group_by(strata) %>%
+  group_modify(~ {
+    n <- nrow(.x)
+    # base sample size is floor(n/2)
+    base_n <- n %/% 2
+    # if odd, flip a coin to add 1
+    extra <- if (n %% 2 == 1 && runif(1) < 0.5) 1 else 0
+    slice_sample(.x, n = base_n + extra)
+  }) %>%
+  ungroup() %>%
+  mutate(treatment = 1L)
 
 treatment_groups <- left_join(strata, samp %>% select(branch_code, treatment), 
           by="branch_code") %>% 
   mutate(group = case_when(treatment==1 ~ "Treatment", 
                            is.na(treatment) ~ "Control")) %>% 
   select(-treatment)
+ 
+print(treatment_groups, n=60)
 
 
 # write_csv(treatment_groups,
