3342b98d485e7adeb0d53912b1d9e7d287427665
Gray <118842255+gcollins99@users.noreply.github.com>
2025-10-06 14:37:14 -0400

    update prerandomization data prep

--- code/phase2/dataprep.R
+++ code/phase2/dataprep.R
@@ -1,5 +1,7 @@
 library(tidyverse)
+library(janitor)
 library(readxl)
+library(openxlsx)
 library(googledrive)
 library(googlesheets4)
 
@@ -10,7 +12,15 @@ library(googlesheets4)
 
 # Directory ------------------------------------------------------------------
 git_dir <- "C:/Users/Gray Collins/Documents/GitHub/kampala-rct"
+drive_path <- "G:/Shared drives/ugandatransit/uganda_transit_archives"
 phase <- "phase2"
+
+
+# # Guide form data ---------------------------------------------------------------
+# load(file.path(drive_path, "dataprocessed/frequency-intervention-otp/guide-form", phase, "guideformdatasets_clean.RDS"), verbose = TRUE)
+# rm(guideform)
+
+
 
 # Create route_hourly dataset ------------------------------------------------------------------
 # Created in guideform_descriptivestats_hfc.Rmd 
@@ -23,23 +33,20 @@ route_hourly_pre <- hwmeans_raw %>%
   mutate(branch_code = str_extract_all(stage, "^(-*)\\d+(?=:)", simplify = T) %>% as.character()) %>% 
   select(park_name, route_id=route_code, route_name, `Time (start hour)`=hr, `Number of observations`=n,
          `Observed average frequency (oaf)`=mean, iqr_oaf=iqr, p25_oaf=p25, p50_oaf=p50, p75_oaf=p75,
-         stage, branch_code)
-
+         stage, branch_code) %>% 
+  # Include only the merger route for 95 
+  filter(route_id == "meng_0001_n" | stage!="95: Mengo-Rubaga-Kosovo-Lusaze")
 
 
 # Route roster dataset
   # Copy from Google Drive to git 
-  stageroster_dir <- "https://docs.google.com/spreadsheets/d/1_cG4STjIpBwmfXpxJtGoye7L1waCCFrMW2PIjK8ljEE/edit?gid=1631865340#gid=1631865340"
-  drive_download(stageroster_dir, path = file.path(git_dir, "data", phase, "Roster of phase 2 stages (descriptive survey take 1).xlsx"),
+  stageroster_dir <- "https://docs.google.com/spreadsheets/d/11XpAT7Y_zJrpe4AO8xbD9Z7NtUJsEiLk/edit?gid=74372498#gid=74372498"
+  drive_download(stageroster_dir, path = file.path(git_dir, "data", phase, "Roster of phase 2 stages (descriptive survey take 1)_v2.xlsx"),
                  overwrite = T)
 
-
-# google drive method: (live doc)
-route_roster_raw <- drive_get(as_id(stageroster_dir)) %>% range_read(sheet = 'route roster') %>% clean_names()
-
-# # git method:
-# stageroster_dir <- file.path(git_dir, "data", phase, "Roster of phase 2 stages (descriptive survey take 1).xlsx")
-# route_roster_raw <- read.xlsx(stageroster_dir, sheet = "route roster") %>% clean_names() 
+# git method:
+stageroster_dir <- file.path(git_dir, "data", phase, "Roster of phase 2 stages (descriptive survey take 1)_v2.xlsx")
+route_roster_raw <- read.xlsx(stageroster_dir, sheet = "route roster_v2") %>% clean_names()
 
 route_roster <- route_roster_raw %>%
   rename(treatment_window=final_treatment_window) %>% 
@@ -49,90 +56,71 @@ route_roster <- route_roster_raw %>%
          window_start = str_split_i(treatment_window, "-", 1),
          window_end = str_split_i(treatment_window, "-", 2), 
          tstart = ifelse(as.numeric(window_start)<=6, as.numeric(window_start)+12, as.numeric(window_start)),
-         tend = ifelse(as.numeric(window_end)<=6, as.numeric(window_end)+12, as.numeric(window_end)))
-
+         tend = ifelse(as.numeric(window_end)<=6, as.numeric(window_end)+12, as.numeric(window_end)),
+         route_code_2 = ifelse(is.na(route_code_2), route_code, route_code_2), 
+         route_name_2 = ifelse(is.na(route_name_2), route_name, route_name_2) %>% str_to_title()) %>%
+  # Filter out routes which are NOT ELIGIBLE for treatment
+  filter(treat_route!="No")
 
 
 # Join treatment window to hrly means dataset, and Calculate hourly payments: 
   # 1. target frequency (by route)  = half of avg freq, rounded to nearest -5 number
   # 2. # of payments (by route) = (p75 headway - target headway) * (14 / p75 headway) 
   # 3. planned # of payments (by route) = # of payments  rounded to nearest whole number 
-route_hourly <- tidylog::inner_join(route_roster %>% select(route_code, strata, treatment_window, tstart, tend, stage_fares, route_fare), 
+route_hourly <- tidylog::inner_join(route_roster %>% select(route_code, route_code_2, route_name_2, strata, 
+                                                            treatment_window, treat_route, tstart, tend, route_fare), 
                                     route_hourly_pre, by = c("route_code"="route_id")) %>% 
   mutate(
     in_treatment_window = case_when(
       `Time (start hour)`>=tstart & `Time (start hour)`<tend ~ 1, 
       .default = 0),
     route_fare = as.numeric(route_fare)) %>% 
-  select(park_name, branch_code, stage, strata, route_code, route_name, 
+  select(park_name, branch_code, stage, strata, route_code, route_name, route_code_2, route_name_2, treat_route,
          treatment_window, `Time (start hour)`, in_treatment_window, `Number of observations`,
          `Observed average frequency (oaf)`, iqr_oaf, p25_oaf, p50_oaf, p75_oaf, 
-         stage_fares, route_fare, tstart, tend) %>% 
-  # Drop rows where NO treatment will be assigned 
-  filter(!is.na(treatment_window)) %>%
-  # Combine routes for relevant stages 
-  mutate(
-    route_code_2 = case_when(
-      route_code %in% c("luga_luga_g", "luga_masa_g", "luga_nase_g") ~ "luga_0001_g", 
-      route_code %in% c("luga_buye_g", "luga_kawu_g", "luga_bujo_g") ~ "luga_0002_g",
-      route_code %in% c("ndej_kana", "ndej_lubu") ~ "ndej_0001", 
-      route_code %in% c("ndej_kibu", "ndej_zant") ~ "ndej_0002",
-      .default = route_code
-    ), 
-    route_name_2 = case_when(
-      route_code_2 == "luga_0001_g" ~ "Lugala / Masanafu / Nasere (MERGED)",
-      route_code_2 == "luga_0002_g" ~ "Buyera-Temangaro / Kawoko / Bujjuko (MERGED)",
-      route_code_2 == "ndej_0001" ~ "Kanaaba / Lubugumu Kakola (MERGED)",
-      route_code_2 == "ndej_0002" ~ "Kibutika / Zanta (MERGED)",
-      .default = route_name
-    ) %>% str_to_title()
-  ) %>% 
-  # Calculate hrly payments (by route)
-  group_by(route_code_2) %>%
-  mutate(
-    target_freq_perhr_raw = `Observed average frequency (oaf)`/2,
-    target_freq_perhr = floor(target_freq_perhr_raw/5)*5,   # round to nearest 5 
-    calculated_payments_perhr = (p75_oaf - target_freq_perhr) * (14/p75_oaf),   # payments per hr 
-    paid_seats_perhr = round(calculated_payments_perhr)   # rounded payments per hr 
-  ) %>% 
-  select(park_name, branch_code, stage, route_code, route_name, route_code_2, route_name_2, everything())
+         route_fare, tstart, tend) %>% 
+  select(park_name, branch_code, stage, route_code, route_name, route_code_2, route_name_2, everything()) %>%
+  filter(in_treatment_window==1)
 
-# # Calculate target frequency by stage (average across all target freqs by stage)
-# stagelvl <- route_hourly %>% 
-#   filter(in_treatment_window==1) %>% 
-#   group_by(stage) %>% 
-#   summarise(target_freq_stage = floor(mean(target_freq_perhr, na.rm=T)/5)*5)
-# 
-# # Calculate target frequency by route (average across all target freqs for single route)
-# routelvl <- route_hourly %>% 
-#   filter(in_treatment_window==1) %>% 
-#   group_by(route_code) %>% 
-#   summarise(target_freq_route = floor(mean(target_freq_perhr, na.rm=T)/5)*5)
-# 
-# # Bind rows & calculate payment targets 
-# route_hourly <- route_hourly %>% 
-#   left_join(stagelvl) %>% 
-#   left_join(routelvl) %>%
-#   mutate(
-#     # Using HOUR- AND ROUTE-SPECIFIC target headway: 
-#     calculated_payments_perhr = (p75_oaf - target_freq_perhr) * (14/p75_oaf),
-#     paid_seats_perhr = round(calculated_payments_perhr),
-#     
-#     # Using ROUTE-wide target headway (same for every hr in a route): 
-#     calculated_payments_route = (p75_oaf - target_freq_route) * (14/p75_oaf),
-#     paid_seats_route = round(calculated_payments_route),
-#     payment_per_departure_route = paid_seats_route * route_fare, 
-#     
-#     # Using STAGE-wide target headway (same for every hr in a stage): 
-#     calculated_payments_stage = (p75_oaf - target_freq_stage) * (14/p75_oaf),
-#     paid_seats_stage = round(calculated_payments_stage),
-#     payment_per_departure_stage = paid_seats_stage * route_fare 
-#   )
+
+# Merger level info
+route_hourly_mergers <- route_hourly %>% 
+  distinct(park_name, branch_code, stage, strata, route_code_2, route_name_2, treat_route,
+           treatment_window, `Time (start hour)`, tstart, tend)
+
+
+# Calculate target frequency by route 
+  # FOR MERGERS: calculate average of mean OAF and p75 OAF for use in target freq & payment calculations   
+route_hourly_mergers <- route_hourly %>% 
+  group_by(park_name, branch_code, stage, strata, route_code_2, route_name_2, 
+           tstart, tend, `Time (start hour)`) %>%
+  summarise(
+    across(.cols = c(`Observed average frequency (oaf)`, p75_oaf),
+           .fns = mean),
+    max_route_fare = max(route_fare, na.rm = TRUE)   # max fare among routes at this stage 
+  )
+
+
+# Calculate target frequency, using the average of OAF and p75 for merged routes
+mergers_target_freq <- route_hourly_mergers %>% 
+  group_by(park_name, branch_code, stage, strata, route_code_2, route_name_2, tstart, tend) %>%
+  summarise(
+    target_freq_route = round(mean(`Observed average frequency (oaf)`/2, na.rm=T)/5)*5,  # avg frequency/2 , rounded to nearest 5
+  )
+
+
+# Calculate payments by route 
+merged_hourly <- tidylog::full_join(route_hourly_mergers, mergers_target_freq) %>% 
+  mutate(
+    calculated_payments = (p75_oaf - target_freq_route) * (14/p75_oaf),   # offset, in # of seats, needed to move a departure from p75 to target time
+    )
 
 
 # Save 
-write_csv(route_hourly %>% select(-tstart, -tend), 
-          file.path(git_dir, "data", phase, "route_hourly.csv"))
+write_csv(route_hourly %>%  select(-tstart, -tend), file.path(git_dir, "data", phase, "route_hourly.csv"))
+          
+write_csv(merged_hourly %>% ungroup() %>% select(-tstart, -tend), 
+          file.path(git_dir, "data", phase, "merged_hourly.csv"))
 
  
 
@@ -147,22 +135,20 @@ write_csv(stage_strata,
 
 
 # Create route-level dataset for mail merge --------------------------------------------------------
-payment_info <- route_hourly %>% 
-  # filter to only rows within the treatment window 
-  filter(in_treatment_window==1) %>% 
-  # average frequency per hour, by stage 
-  group_by(stage) %>% 
-  mutate(target_freq_stage = floor(mean(target_freq_perhr, na.rm=T)/5)*5) %>% 
-  # average frequency per hour, by route 
-  group_by(park_name, branch_code, stage, strata, route_code_2, route_name_2, route_fare, tstart, tend, target_freq_stage) %>% 
-  summarise(paid_seats = mean(paid_seats_perhr, na.rm=T),
-            target_freq_route =  floor(mean(target_freq_perhr, na.rm=T)/5)*5) %>% 
-  mutate(payment_value = route_fare * round(paid_seats), 
-         stage = str_remove(stage, "^\\d*: "),
+# Calculate # of paid seats 
+merged_paid_seats <- merged_hourly %>% 
+  group_by(route_code_2) %>% 
+  summarise(
+    max_route_fare = max(max_route_fare),
+    paid_seats = round(mean(calculated_payments, na.rm=T)),  # rounded average across 4-hr period    
+    payment_value = paid_seats * max_route_fare  # fare * estimated # of seats paid per hour
+  )
+
+# Join with payment_info 
+payment_info <- tidylog::full_join(mergers_target_freq, merged_paid_seats) %>% 
+  mutate(stage = str_remove(stage, "^\\d*: "),
          tstart = as.character(tstart) %>% str_c(.,":00"),
-         tend = as.character(tend) %>% str_c(.,":00")) %>% 
-  # drop routes 
-  filter(route_code_2 != "ndej_nyan")
+         tend = as.character(tend) %>% str_c(.,":00")) 
 
 write_csv(payment_info, 
           file.path(git_dir, "data", phase, "payment_info.csv"))
